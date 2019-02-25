6dfc4405124f376c7e86acf25181ae89c1a52a91
mkrolak <mkrolakk@gmail.com>
2019-02-25 13:21:40 -0600

    Made basic layout for changes profile. have not made the stuff for changing the users profile picture though.

--- app/src/main/java/com/mkrolak/EditProfileActivity.java
+++ app/src/main/java/com/mkrolak/EditProfileActivity.java
@@ -2,12 +2,24 @@ package com.mkrolak;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+    public void resetPassword(View v){
+        mAuth.sendPasswordResetEmail(mAuth.getCurrentUser().getEmail());
+
     }
 }
